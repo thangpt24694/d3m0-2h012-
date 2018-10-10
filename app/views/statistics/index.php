b388fe498926b11f6689b17dde59dec7d4467e1b
thangpt <thanhthangqt@gmail.com>
2018-10-10 21:15:36 +0700

    add project

---
+++ app/views/statistics/index.php
@@ -0,0 +1,46 @@
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <!--<div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div> -->
+      <div class="modal-body">
+        <div class="">
+          <div class="row">
+
+            <div class="main">
+
+              <h3 class="text-center">Please Log In</h3>
+              <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                  <a href="#" class="btn btn-lg btn-primary btn-block">Facebook</a>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                  <a href="#" class="btn btn-lg btn-danger btn-block">Google</a>
+                </div>
+              </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<script type="text/javascript">
+ $(document).ready(function(){
+    $("#myModal").modal();
+});
+</script>
